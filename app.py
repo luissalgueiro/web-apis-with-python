a3a6e5f5bd334334f2f68e4b136cb9e7aef5921e
Luis Salgueiro <luis.salgueiro.py@gmail.com>
2023-01-18 14:39:40 -0300

    adding first ToDo app

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify, request
+
+### initialize the app ###
+app = Flask(__name__)
+
+### Define what the app does ###
+@app.get('/greet')
+def index():
+    """
+    ToDo: 
+    1. Capture first name and last name
+    2. If either is not provided, return an error
+    3. If first name is not provided and second name is provided, respond with "Hello, Mr/Miss <SecondName>!"
+    4. If first name is provided and second name is not provided, respond with "Hello,  <FirstName>!"
+    5. If both names are provided, respond with a question: "Is <FirstName> and <SecondName> your names?"
+    
+    """
+    return jsonify("TODO")
+
