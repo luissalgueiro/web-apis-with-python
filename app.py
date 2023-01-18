305c20fc616db20163e04be17bee559ce2f21bd3
Luis Salgueiro <luis.salgueiro.py@gmail.com>
2023-01-18 15:27:00 -0300

    responses for the first to do

--- app.py
+++ app.py
@@ -6,15 +6,15 @@ app = Flask(__name__)
 ### Define what the app does ###
 @app.get('/greet')
 def index():
-    ## adding name as param for the API
-    name = request.args.get('name')
-    ## adding an error response in case the argument was not given properly
-    if not name:
-        return jsonify({'status':'Error!'})
-    response = {'data':f'Hello {name}!!'}
-    ## When this direction is passing to the web browser, it generates the correct response
-    ### http://127.0.0.1:5000/greet?name=Luis 
-    ## ?var_name=var_value is the correct syntax
+    # ## adding name as param for the API
+    # name = request.args.get('name')
+    # ## adding an error response in case the argument was not given properly
+    # if not name:
+    #     return jsonify({'status':'Error!'})
+    # response = {'data':f'Hello {name}!!'}
+    # ## When this direction is passing to the web browser, it generates the correct response
+    # ### http://127.0.0.1:5000/greet?name=Luis 
+    # ## ?var_name=var_value is the correct syntax
     
     """
     ToDo: 
@@ -25,8 +25,16 @@ def index():
     5. If both names are provided, respond with a question: "Is <FirstName> and <SecondName> your names?"
     
     """
+    fname = request.args.get('fname')
+    lname = request.args.get('lname')
+    if not fname and lname:
+        return jsonify({'status':'Error!'})
+    elif lname and not fname:
+        return jsonify({"data":f"Hello {lname}"})
+    elif fname and not lname:
+        return jsonify({"data":f"Hello {fname}"})
+    else:
+        return jsonify({"data":f" Is {fname} and {lname} your names?"})
     
-    # response = {"data":"Hello World!!!"}
-    
-    return jsonify(response)
+    # return jsonify(response)
 
