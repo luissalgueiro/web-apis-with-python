c45d085ab43e851cdc48c61ec704a666593d81a6
Luis Salgueiro <luis.salgueiro.py@gmail.com>
2023-01-19 19:31:36 -0300

    adding functionalities to the app.py, to accept the file image and call the filter method to return a filtered image

--- app.py
+++ app.py
@@ -8,6 +8,12 @@ from bin.filters import apply_filter
 app = Flask(__name__)
 
 ## READ PIL documentation to find out the filters available
+filters_available=[
+    "blur",
+    "contour",
+    'edge_enhance',
+    'edge_enhance_more'
+]
 
 
 
@@ -19,10 +25,16 @@ def index():
     ToDo:
     1. Return the usage instructions that specifies which filters are available and the method format
     """
-    pass
+    response = {
+        "filters_available":filters_available,
+        "usage":{
+            "http_method":"POST",
+            "URL":"/<filters_available>"
+        },
+    }
+    return jsonify(response)
 
-    # return render_template("index.html")
-
+    
 @app.post('/<filter>')
 def image_filter(filter):
     """
@@ -32,6 +44,18 @@ def image_filter(filter):
     3. Apply filter using apply_filter method imported from bin.filters 
     4. Return the filtered image
     """
+    if filter not in filters_available:
+        response = {"Error":"Unavailable filter"}
+        return jsonify(response)
+    
+    file = request.files['image']
+    if not file:
+        response = {"Error":'File not found!'}
+        return jsonify(response)
+    
+    filtered_image = apply_filter(file, filter)
+    
+    return send_file(filtered_image, mimetype="image/JPEG")
    
 
 if __name__=='__main__':
