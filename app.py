b2bb466221e88426586221ed72364377cd75192c
Luis Salgueiro <luis.salgueiro.py@gmail.com>
2023-01-19 09:58:11 -0300

    formating app.py for the new web browser project

--- app.py
+++ app.py
@@ -1,40 +1,20 @@
-from flask import Flask, jsonify, request
+## Project 2 - Web Browser Google alike using the Google API 
+
+
+from flask import Flask, request, render_template, redirect
 
 ### initialize the app ###
 app = Flask(__name__)
 
 ### Define what the app does ###
-@app.get('/greet')
+@app.get('/')
 def index():
-    # ## adding name as param for the API
-    # name = request.args.get('name')
-    # ## adding an error response in case the argument was not given properly
-    # if not name:
-    #     return jsonify({'status':'Error!'})
-    # response = {'data':f'Hello {name}!!'}
-    # ## When this direction is passing to the web browser, it generates the correct response
-    # ### http://127.0.0.1:5000/greet?name=Luis 
-    # ## ?var_name=var_value is the correct syntax, this is called the querystring.
-    
-    """
-    ToDo: 
-    1. Capture first name and last name
-    2. If either is not provided, return an error
-    3. If first name is not provided and second name is provided, respond with "Hello, Mr/Miss <SecondName>!"
-    4. If first name is provided and second name is not provided, respond with "Hello,  <FirstName>!"
-    5. If both names are provided, respond with a question: "Is <FirstName> and <SecondName> your names?"
-    
-    """
-    fname = request.args.get('fname')
-    lname = request.args.get('lname')
-    if not fname and not lname:
-        return jsonify({'status':'Error!'})
-    elif lname and not fname:
-        return jsonify({"data":f"Hello {lname}"})
-    elif fname and not lname:
-        return jsonify({"data":f"Hello {fname}"})
-    else:
-        return jsonify({"data":f" Is {fname} and {lname} your names?"})
-    
-    # return jsonify(response)
+    return render_template('index.html')
 
+@app.get('/search')
+def search():
+    args = request.args.get('q')
+    return redirect(f'https://www.google.com/search?q={args}')
+
+if __name__=='__main__':
+    app.run()
