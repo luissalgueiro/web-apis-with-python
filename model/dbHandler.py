3b761a131fe84b0ea7012889f868ea49b23b7527
Luis Salgueiro <luis.salgueiro.py@gmail.com>
2023-01-19 14:22:38 -0300

    adding template for the new project, with db

---
+++ model/dbHandler.py
@@ -0,0 +1,37 @@
+import sqlite3 as SQL
+
+def match_exact(word:str)->list:
+    """
+    This method will:
+    1. Accept string
+    2. Search the dictionary for the exact match
+    3. If success, return the definition
+    4. If not success, return empty list
+
+    Args:
+        word (str): word that will be searched
+
+    Returns:
+        list: definition of the word 
+    """
+    
+    ## ToDo: Establish connection to the dictionary database
+    ## ToDo: Query the database for the exact matches.
+    ## ToDo: Clone the connection to the database
+    
+    return "ToDo"
+
+def match_like(word:str) -> list:
+    """
+    
+    Args:
+        word (str): input word to be searched.
+    Returns:
+        list: if success, return description of the list
+                if not, return empty list
+    """
+    ## ToDo: Establish connection to the dictionary database
+    ## ToDo: Query the database for the exact matches.
+    ## ToDo: Clone the connection to the database
+    
+    return "ToDo"
