83b339383bfed07a465a6447171a17084c6d4421
Luis Salgueiro <luis.salgueiro.py@gmail.com>
2023-01-19 19:38:38 -0300

    adding the code for the apply_filter method

--- bin/filters.py
+++ bin/filters.py
@@ -10,3 +10,11 @@ def apply_filter(file: object, filter: str) -> object:
       4. Convert the PIL image object into a file object
       5. return the file object
     """
+    image = Image.open(file)
+    image = image.filter(eval(f"ImageFilter.{filter.upper()}"))
+    file = io.BytesIO()
+    image.save(file,'JPEG')
+    file.seek(0)
+    
+    return file
+    
