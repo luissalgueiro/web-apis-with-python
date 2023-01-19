98d49a5775dc2e8e88a4e87599da64f316228c02
Luis Salgueiro <luis.salgueiro.py@gmail.com>
2023-01-19 18:55:05 -0300

    initial template for the app.py, for this new project

--- app.py
+++ app.py
@@ -1,50 +1,38 @@
-## Project 3 - Dictionary API with sqlite db 
+## Project 4 - Image Filter API 
 
-from flask import Flask, request, jsonify, render_template
-from model.dbHandler import match_exact, match_like
+from flask import Flask, request, jsonify, send_file
+# from model.dbHandler import match_exact, match_like
+from bin.filters import apply_filter
 
 ### initialize the app ###
 app = Flask(__name__)
 
+## READ PIL documentation to find out the filters available
+
+
+
 ### Define what the app does ###
-@app.get('/')
+@app.route('/', methods=["GET","POST"])
+
 def index():
     """
-    Provide instructions for the usage 
-    Returns:
-        response: json formatted
+    ToDo:
+    1. Return the usage instructions that specifies which filters are available and the method format
     """
-    # response =  {"usage":f"/dict?=<word>"}
-    # return jsonify(response)
-    return render_template("index.html")
+    pass
 
-@app.get('/dict')
-def dictionary():
-    
-    words = request.args.getlist('word')
-    
-    if not words:
-        response = {"status":"Error",'words':words, 'data':"word not found"}
-        return jsonify(response)
-    
-    response = {'words':[]}
-    
-    for word in words:
-        definitions = match_exact(word)
-        if definitions:
-            response['words'].append({"status":"Success","data":definitions,'word':word})
-            # return jsonify(response)
-        else:
-            definitions = match_like(word)
-            if definitions:
-                response['words'].append({"status":"Partial","data":definitions,'word':word})
-                # return jsonify(response)
-            else:
-                response['words'].append({"Status":"Error","data":"word not found"})
-                # return jsonify(response)
-    # return jsonify(response)     
-    return render_template('results.html',response=jsonify(response))   
+    # return render_template("index.html")
 
+@app.post('/<filter>')
+def image_filter(filter):
+    """
+    ToDo: 
+    1. Check if the provided filter is available,if not, return error
+    2. Check if a file has been provided in the POST request., if not, return error
+    3. Apply filter using apply_filter method imported from bin.filters 
+    4. Return the filtered image
+    """
+   
 
 if __name__=='__main__':
     app.run()
