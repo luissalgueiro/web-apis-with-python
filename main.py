645adcc0c9d0b55a94b22bd6878f54c20e2f0a9b
Luis Salgueiro <luis.salgueiro.py@gmail.com>
2023-01-20 08:26:57 -0300

    reformating the code to work with fastapi

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
 from fastapi import FastAPI
+from fastapi import jsonable_encoder
+from typing import Optional
+from model.dbHandler import match_exact, match_like
+
+
 
 app = FastAPI()
 
 
 @app.get('/')
 def index():
-    response = {"Hello":"FastAPI"}
-    return response
+    # response = {"Hello":"FastAPI"}
+    response = {'usage':'/dict?=<word>'}
+    return jsonable_encoder(response)
+
+@app.get('/dict')
+def dict(word:str):
+    if not word:
+        response = {'status':'Error','word':word,'data':'Word Not Found!'}
+        return jsonable_encoder(response)
+    definitions = match_exact(word)
+    if definitions:
+        response = {'status':'success','word':word,'data':definitions}
+        return jsonable_encoder(response)
+    else:
+        definitions = match_like(word)
+        if definitions:
+            response = {'status':'partial','word':word,'data':definitions}
+            return jsonable_encoder(response)
+        else:
+            response = {'status':'Error','word':word,'data':"Word without definition"}
+            return jsonable_encoder(response)
+    
