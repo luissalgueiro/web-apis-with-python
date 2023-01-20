b3d327f71b86da45ede83491bf2d90a75696bf2b
Luis Salgueiro <luis.salgueiro.py@gmail.com>
2023-01-20 19:21:03 -0300

    fastAPI formating for filtering image{

--- main.py
+++ main.py
@@ -1,34 +1,38 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from fastapi.encoders import jsonable_encoder
-from typing import Optional
+from fastapi.responses  import StreamingResponse
+from typing import List, Optional
 from model.dbHandler import match_exact, match_like
-
+import io
+from bin.filters import apply_filter
 
 
 app = FastAPI()
 
 
-@app.get('/')
+filters_available=[
+    "blur",
+    "contour",
+    "detail",
+    "edge_enhance",
+]
+
+
+@app.api_route('/', methods=["GET","RESPONSE"])
 def index():
     # response = {"Hello":"FastAPI"}
-    response = {'usage':'/dict?=<word>'}
+    response = {"filters_available":filters_available,
+                "usage":{
+                    "http_method":"POST",
+                    "URL":"/<filter_available>/",
+                }}
     return jsonable_encoder(response)
 
-@app.get('/dict')
-def dict(word:str):
-    if not word:
-        response = {'status':'Error','word':word,'data':'Word Not Found!'}
+@app.post('/{filter}')
+def image_filter(filter:str, img:UploadFile=File(...)):
+    if filter not in filters_available:
+        response = {"Error":"Filter not available"}
         return jsonable_encoder(response)
-    definitions = match_exact(word)
-    if definitions:
-        response = {'status':'success','word':word,'data':definitions}
-        return jsonable_encoder(response)
-    else:
-        definitions = match_like(word)
-        if definitions:
-            response = {'status':'partial','word':word,'data':definitions}
-            return jsonable_encoder(response)
-        else:
-            response = {'status':'Error','word':word,'data':"Word without definition"}
-            return jsonable_encoder(response)
     
+    filtered_image = apply_filter(img.file, filter)
+    return StreamigResponse(filtered_image, media_type="image/jpeg")
